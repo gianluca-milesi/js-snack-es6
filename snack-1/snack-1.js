55adacda402a46c8dd9b9437a8f3e5d4bc3db137
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2024-10-27 15:47:59 +0100

    ADD: bicycle objects in bikes array

--- snack-1/snack-1.js
+++ snack-1/snack-1.js
@@ -1,1 +1,22 @@
-console.log("ciao");
+/*
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare a schermo la bici con peso minore.
+*/
+
+bikesArray = [
+    {
+        name: "bike-1",
+        weight: 15
+    },
+    {
+        name: "bike-2",
+        weight: 17
+    },
+    {
+        name: "bike-3",
+        weight: 10
+    }
+];
+
+console.log(bikesArray[0].weight);
