a46a4113611ba9d13b3dfe11508991aa8b9410f5
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2024-10-27 16:26:31 +0100

    ADD: function and for cycle to generate and add random numbers to objects properties

--- snack-2/snack-2.js
+++ snack-2/snack-2.js
@@ -1,1 +1,38 @@
-console.log("ciao");
+/*
+Creare un array di oggetti di squadre di calcio.
+Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+*/
+
+soccerTeams = [
+    {
+        name: "Roma",
+        scoredPoints: 0,
+        foulsSuffered: 0
+    },
+    {
+        name: "Milan",
+        scoredPoints: 0,
+        foulsSuffered: 0
+    },
+    {
+        name: "Inter",
+        scoredPoints: 0,
+        foulsSuffered: 0
+    },
+];
+
+function generateRandomNumber() {
+    let randomNumber = Math.floor(Math.random() * 101);
+    return randomNumber;
+}
+
+for (let i = 0; i < soccerTeams.length; i++) {
+    console.log(soccerTeams[i]);
+
+    soccerTeams[i].scoredPoints += generateRandomNumber();
+    soccerTeams[i].foulsSuffered += generateRandomNumber();
+}
+console.log(soccerTeams);
