458ffe25cfcfe84b594192ca2d2cf5b606058501
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2024-10-27 16:40:53 +0100

    ADD: new array and for loop that pushes and prints names and fouls suffered

--- snack-2/snack-2.js
+++ snack-2/snack-2.js
@@ -3,7 +3,6 @@ Creare un array di oggetti di squadre di calcio.
 Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
 Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
 Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
-Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 */
 
 soccerTeams = [
@@ -24,6 +23,7 @@ soccerTeams = [
     },
 ];
 
+
 function generateRandomNumber() {
     let randomNumber = Math.floor(Math.random() * 101);
     return randomNumber;
@@ -36,3 +36,31 @@ for (let i = 0; i < soccerTeams.length; i++) {
     soccerTeams[i].foulsSuffered += generateRandomNumber();
 }
 console.log(soccerTeams);
+
+
+//Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+/* prova:
+let nuovoOggetto = {
+    name: soccerTeams[i].name,
+    foulsSuffered: soccerTeams[i].foulsSuffered
+};
+namesFouls.push(nuovoOggetto);
+*/
+
+namesFouls = [];
+
+for (let i = 0; i < soccerTeams.length; i++) {
+    namesFouls.push({
+        name: soccerTeams[i].name,
+        foulsSuffered: soccerTeams[i].foulsSuffered
+    });
+}
+
+console.log("Solo nomi e falli subiti", namesFouls);
+
+
+
+
+
+
+
